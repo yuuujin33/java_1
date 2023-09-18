70b30ee4e4a82f28d861cf5a5efa4d1d5f33802f
yuuujin33 <yuuujin33@gmail.com>
2023-09-18 11:40:07 +0900

    set

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,11 +17,9 @@ dependencies {
 tasks.test {
     useJUnitPlatform()
 }
-fun dependencies() {
-    testImplementation
-    'org.junit.jupiter:junit-jupiter-api:5.8.1'
-    testRuntimeOnly
-    'org.junit.jupiter:junit-jupiter-engine:5.8.1'
-    testImplementation
-    'org.assertj:assertj-core:3.11.1' // assertThat 을 쓸 수 있게 해주는 라이브러리
-}
+dependencies {
+        testImplementation 'org.junit.jupiter:junit-jupiter-api:5.8.1'
+        testRuntimeOnly 'org.junit.jupiter:junit-jupiter-engine:5.8.1'
+
+        testImplementation 'org.assertj:assertj-core:3.11.1' // assertThat 을 쓸 수 있게 해주는 라이브러리
+    }
