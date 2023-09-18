82bfd5f0a7455b3cedf0a614f548747b47807a7f
yuuujin33 <yuuujin33@gmail.com>
2023-09-18 11:10:28 +0900

    커ë°depend

---
+++ src/test/java/test.java
@@ -0,0 +1,8 @@
+public class test {
+}
+    dependencies {
+        testImplementation 'org.junit.jupiter:junit-jupiter-api:5.8.1'
+        testRuntimeOnly 'org.junit.jupiter:junit-jupiter-engine:5.8.1'
+
+        testImplementation 'org.assertj:assertj-core:3.11.1' // assertThat 을 쓸 수 있게 해주는 라이브러리
+    }
