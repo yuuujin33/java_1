0e1d75334e5524c42ca4c12461ffd81439a4eb87
yuuujin33 <yuuujin33@gmail.com>
2023-09-20 11:58:23 +0900

    프로class

---
+++ src/main/java/org/example/Car.java
@@ -0,0 +1,39 @@
+package org.example;
+
+//public class Car {
+//    int speed;
+//    void run () {
+//        System.out.println("달리다.");   }
+//}
+
+//public class Car {
+//    void run () {
+//        System.out.println("달리다.");
+//    }
+//
+//}
+
+//class Car {
+//    // 구현
+//    int speed;
+//    void run () {
+//        System.out.println("자동차가 최고속력" +  speed  + "로 달립니다.");
+//    }
+//}
+//
+//class Car {
+//    // 구현
+//    int num;
+//    int speed;
+//
+//    void run () {
+//        System.out.println(num + "번 자동차가" + speed + "km 로 달립니다.");
+//    }
+//}
+
+public class Car {
+    // 구현
+    static void run () {
+        System.out.println("자동차가 달립니다.");
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,73 @@
 package org.example;
 
+//class Main {
+//    public static void main(String[] args) {
+//        Car car = new Car();
+//        car.run();
+//        car.run();
+//        car.run();
+//    }
+//}
 
-public class Main {
+// 문제 : 3개의 자동차가 각각 1번씩 달리게 해주세요.
+
+//class Main {
+//    public static void main(String[] args) {
+//        Car car1 = new Car();
+//        Car car2 = new Car();
+//        Car car3 = new Car();
+//        car1.run();
+//        car2.run();
+//        car3.run();
+//    }
+//}
+
+// 문제 : 각각의 자동차가 서로 다른 최고속력으로 달리게 해주세요.
+
+//class Main {
+//    public static void main(String[] args) {
+//
+//        // 구현시작
+//        Car a자동차1 = new Car();
+//        a자동차1.speed = 230;
+//        Car a자동차2 = new Car();
+//        a자동차2.speed = 210;
+//        // 구현끝
+//
+//        a자동차1.run();
+//        //출력 => 자동차가 최고속력 230km로 달립니다.
+//
+//        a자동차2.run();
+//        // 출력 => 자동차가 최고속력 210km로 달립니다.
+//    }
+//}
+
+// 문제 : 번호가 다른 각각의 자동차가 서로 다른 최고속력으로 달리게 해주세요.
+
+//class Main {
+//    public static void main(String[] args) {
+//
+//        // 구현시작
+//        Car.car1 = new Car();
+//        car1.num = 1;
+//        car1.speed = 230;
+//        Car.car2 = new Car();
+//        car2.num = 2;
+//        car2.speed = 210;
+//        // 구현끝
+//
+//        car1.run();
+//        // 출력 => 1번 자동차가 최고속력 230km로 달립니다.
+//
+//        car2.run();
+//        // 출력 => 2번 자동차가 최고속력 210km로 달립니다.
+//    }
+//}
+
+// 문제 : 아래 코드가 실행되도록 자동차 클래스를 만들어주세요.
+
+class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+        Car.run();
+    }
+}
